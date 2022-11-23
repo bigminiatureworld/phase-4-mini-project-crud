39b1e2361c96581bb0381c22692dfc5ca95639eb
Adam Weaver <adamweaver90@gmail.com>
2022-11-23 10:32:47 -0500

    commit tree 55ff28fc5b99fbcf5f8af02e66f5bc46e9605724

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_221510) do
+ActiveRecord::Schema.define(version: 2022_11_23_124540) do
 
   create_table "spices", force: :cascade do |t|
     t.string "title"
